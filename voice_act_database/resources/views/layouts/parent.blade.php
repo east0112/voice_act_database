647661c42aa2a712f64427ba518f294fa8cd9fc4
east5991 <magica.mk.38@gmail.com>
2019-07-22 23:54:01 +0900

    2019/07/22 add
    構成管理対象追加

--- voice_act_database/resources/views/layouts/parent.blade.php
+++ voice_act_database/resources/views/layouts/parent.blade.php
@@ -6,7 +6,7 @@
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<link href="{{ asset('css/parent.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/parent.css') }}" rel="stylesheet">
 
 <head>
     <meta charset="UTF-8">
