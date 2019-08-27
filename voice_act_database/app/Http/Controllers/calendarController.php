05efe9f705e7b48097cf804ab2571e56dbde393a
east5991 <magica.mk.38@gmail.com>
2019-08-28 01:08:46 +0900

    update
    calendar default

--- voice_act_database/app/Http/Controllers/calendarController.php
+++ voice_act_database/app/Http/Controllers/calendarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Models\common;
+use App\Http\Models\getDatabase;
 
 class calendarController extends Controller
 {
@@ -17,7 +18,12 @@ class calendarController extends Controller
     public function searchDisplay(Request $request){
       //端末判定
       $view = common::getDevice($request,"calendar");
+      //当月設定
+      $currentMonth = "8";
 
-      return view($view);
+      //カレンダー情報の取得
+      $dates = getDatabase::getCalendarDates("2019",$currentMonth);
+
+      return view($view,["dates"=>$dates,"currentMonth"=>$currentMonth]);
     }
 }
--- voice_act_database/resources/views/pc/calendar.blade.php
+++ voice_act_database/resources/views/pc/calendar.blade.php
@@ -9,6 +9,32 @@
                 <h4>カレンダー検索</h4>
                 <p>イベントやラジオ、雑誌等の出演情報をカレンダーから検索できます。</p>
                 </br>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                        @foreach (['日', '月', '火', '水', '木', '金', '土'] as $dayOfWeek)
+                        <th>{{ $dayOfWeek }}</th>
+                        @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($dates as $date)
+                        @if ($date->dayOfWeek == 0)
+                        <tr>
+                        @endif
+                        <td
+                            @if ($date->month != $currentMonth)
+                            class="bg-secondary"
+                            @endif
+                        >
+                            {{ $date->day }}
+                        </td>
+                        @if ($date->dayOfWeek == 6)
+                        </tr>
+                        @endif
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
