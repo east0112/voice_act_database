c9142ab07937ee7f787cad7b0f8302eae5aead70
豊島大地 <toyoshimadaichi@Macbook-for-east.local>
2019-09-04 00:53:41 +0900

    update
    calendar：モーダルの背景スクロール対応

--- voice_act_database/public/js/calendar.js
+++ voice_act_database/public/js/calendar.js
@@ -3,12 +3,14 @@ $(function () {
     $(".js-view-detail").click(function(){
       modalId = event.target.id;
       if($(".item" + modalId).length){
+        $("body").css("position","fixed");
         $(".item" + modalId).show();
         $(".modal").fadeIn();  
       }
       return false;
     });
     $(".js-modal-close").click(function(){
+      $("body").css("position","static");
       $(".modal").fadeOut()
       $(".item" + modalId).fadeOut();
       return false;
