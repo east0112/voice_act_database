c477abafd2621d1335297c77e0937834b3fade29
豊島大地 <toyoshimadaichi@Macbook-for-east.local>
2019-09-03 22:10:21 +0900

    update
    calendar：ページャーのレイアウト修正

--- voice_act_database/resources/views/pc/calendar.blade.php
+++ voice_act_database/resources/views/pc/calendar.blade.php
@@ -49,7 +49,7 @@
                     <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
                         <div class="calendar-header">
                             <div class="row">
-                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 left-button">
                                     <form action="/calendar" method="GET">
                                         <input type="hidden" name="currentYear" value={{$currentYear}}>
                                         <input type="hidden" name="currentMonth" value={{$currentMonth}}>
@@ -57,12 +57,12 @@
                                         <button class="pager-button" type="submit" name="action">◀︎</button>
                                     </form>
                                 </div>
-                                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                                     <div class="calendar-header-date">
                                     {{$currentYear}} . {{$currentMonth}}
                                     </div>
                                 </div>
-                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 right-button">
                                     <form action="/calendar" method="GET">
                                         <input type="hidden" name="currentYear" value={{$currentYear}}>
                                         <input type="hidden" name="currentMonth" value={{$currentMonth}}>
