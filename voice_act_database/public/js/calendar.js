8ee9d47adb150c3632607c3c5bf8350b5a2abf25
豊島大地 <toyoshimadaichi@Macbook-for-east.local>
2019-09-04 01:16:03 +0900

    update

--- voice_act_database/public/js/calendar.js
+++ voice_act_database/public/js/calendar.js
@@ -3,14 +3,17 @@ $(function () {
     $(".js-view-detail").click(function(){
       modalId = event.target.id;
       if($(".item" + modalId).length){
-        $("body").css("position","fixed");
+        //$("body").css("position","fixed");
+        $("body").css("overflow","hidden");
+        //$("body").css("top",$(window).scrollTop() & "px");
         $(".item" + modalId).show();
         $(".modal").fadeIn();  
       }
       return false;
     });
     $(".js-modal-close").click(function(){
-      $("body").css("position","static");
+      //$("body").css("position","static");
+      $("body").css("overflow","auto");
       $(".modal").fadeOut()
       $(".item" + modalId).fadeOut();
       return false;
