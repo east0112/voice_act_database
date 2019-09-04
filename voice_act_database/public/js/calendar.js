4f7e225009ece19fdd3ec018b005ae78c23dd601
豊島大地 <toyoshimadaichi@Macbook-for-east.local>
2019-09-05 01:05:29 +0900

    update
    calendar：モーダルの背景スクロール対応（iphone）

--- voice_act_database/public/js/calendar.js
+++ voice_act_database/public/js/calendar.js
@@ -1,18 +1,23 @@
 $(function () {
   var modalId ;
+  var movefun = function( event ){
+    event.preventDefault();
+  }
     $(".js-view-detail").click(function(){
       modalId = event.target.id;
       if($(".item" + modalId).length){
-        //$("body").css("position","fixed");
+        // スクロール停止
         $("body").css("overflow","hidden");
-        //$("body").css("top",$(window).scrollTop() & "px");
+        window.addEventListener( 'touchmove' , movefun , { passive: false } );
+ 
         $(".item" + modalId).show();
         $(".modal").fadeIn();  
       }
       return false;
     });
     $(".js-modal-close").click(function(){
-      //$("body").css("position","static");
+      // スクロール開始
+      window.removeEventListener( 'touchmove' , movefun , { passive: false } );
       $("body").css("overflow","auto");
       $(".modal").fadeOut()
       $(".item" + modalId).fadeOut();
