0ed1f6d78b77ba08cfcba15577526d516ee245de
豊島大地 <toshimadaichi@toshimadaichinoMacBook-Pro.local>
2020-03-06 23:59:32 +0900

    Vue.jsへ切り替え＆グランドデザイン刷新の仕掛け

---
+++ voice_act_database/resources/js/components/HeaderComponent.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="header-nav">
+        <div class="header-nav__content">
+            <a href="/">
+                <div class="header-nav__item">
+                    <span>HOME</span>
+                </div>
+            </a>
+            <a href="/about">
+                <div class="header-nav__item">
+                    <span>ABOUT</span>
+                </div>
+            </a>
+            <a href="/library">
+                <div class="header-nav__item">
+                    <span>EVENTS</span>
+                </div>
+            </a>
+            <a href="/tickets">
+                <div class="header-nav__item">
+                    <span>TICKETS</span>
+                </div>
+            </a>
+            <a href="/calendar">
+                <div class="header-nav__item">
+                    <span>CALENDAR</span>
+                </div>
+            </a>
+        </div>
+        <i class="material-icons header-nav__button" v-on:click="toggleMenuStatus">menu</i>
+        <div v-bind:class="{ 'active': status }">aaaaaaaaaaaaaaa</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: '#header-nav',
+  data: function(){
+    return{
+        status: false
+        }
+  },
+  methods: {
+    toggleMenuStatus: function (event) {
+        this.status = !this.status;
+    }
+  }
+}
+</script>
+
+<style>
+    #header-nav{
+        height:56px;
+    }
+    .header-nav__content{
+        font-size:0.9rem;
+        line-height:56px;
+        display:flex;
+        margin:0 auto;
+        width:992px;
+    }
+    .header-nav__item{
+        color:#fff;
+        padding:0 20px;
+    }
+    @media screen and (min-width:993px) { 
+    .header-nav__button{
+        display:none;
+    }    
+    }
+    @media screen and (max-width:992px) { 
+    #header-nav{
+        position: relative;
+    }
+    .header-nav__content{
+        display:none;
+    }
+    .header-nav__button{
+        position: absolute;
+        right:0.5rem;
+        color:#fff;
+        line-height:56px;
+        font-size:2rem;
+        cursor:pointer;
+        padding:0 1rem;
+    }
+}
+
+</style>
--- voice_act_database/resources/views/layouts/parent.blade.php
+++ voice_act_database/resources/views/layouts/parent.blade.php
@@ -2,12 +2,13 @@
 <html lang="ja">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="description" content="伊波杏樹さんの出演情報をまとめた非公式ファンサイトです。"/>
 <meta name="keywords" content="SMA声優, SMA VOICE,ファンクラブ">
 <meta property="og:type" content="website"/>
 <meta property="og:description" content="伊波杏樹さんの出演情報をまとめた非公式ファンサイトです。"/>
 <meta property="og:site_name" content="inaminfo"/>
-<meta property="og:image" content="images/home001.png"/>
+<meta property="og:image" content="{{ asset('/images/home001.png') }}"/>
 <meta property="og:locale" content="ja_JP" />
 <meta property="og:type" content="website" />
 <meta name="twitter:card" content="summary"/>
@@ -16,22 +17,16 @@
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" />
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.1/css/hover.css">
 
-<link href="{{ asset('/css/pc/parent.css') }}" rel="stylesheet">
-<link href="{{ asset('/css/common.css') }}" rel="stylesheet">
+<!--<link href="{{ asset('/css/pc/parent.css') }}" rel="stylesheet">-->
+<!--<link href="{{ asset('/css/common.css') }}" rel="stylesheet">-->
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 <script src="{{ asset('/js/parent.js') }}"></script>
 
 <head>
@@ -40,84 +35,58 @@
 </head>
 <body>
 <!--mobile nav-menu-->
-<p id="page-top"><a href="#wrap">PAGE TOP</a></p>
-<div class="mobile-nav">
-    <div class="mobile-nav-bg js-mobile-nav-close"></div>
-        <div class="mobile-nav-content">
-            <div class="row">
-              <a href="/"><div class="nav-item nav-item-1 <?php if( $_SERVER['REQUEST_URI'] == "/") echo "active"; ?>">
-              <p>HOME</p>
-              </div></a><br />
-              <a href="/about"><div class="nav-item nav-item-2 <?php if( strpos($_SERVER['REQUEST_URI'],"/about") !== false)  echo "active"; ?>">
-              <p>ABOUT</p>
-              </div></a><br />
-              <a href="/library"><div class="nav-item nav-item-3 <?php if( strpos($_SERVER['REQUEST_URI'],"/library") !== false)  echo "active"; ?>">
-              <p>LIBRARY</p>
-              </div></a><br />
-              <a href="/calendar"><div class="nav-item nav-item-4 <?php if( strpos($_SERVER['REQUEST_URI'],"/calendar") !== false)  echo "active"; ?>">
-              <p>CALENDAR</p>
-              </div></a>
-            </div>
-        <!--<a class="js-mobile-nav-close mobile-nav-close">閉じる</a>-->
-        </div>
-    </div>
-</div>
-<!--mobile nav-menu-->
+<!-- <p id="page-top"><a href="#wrap">PAGE TOP</a></p> -->
+<div id="app">
+  <header-component></header-component>
+<!--  <div class="mobile-nav">
+      <div class="mobile-nav-bg js-mobile-nav-close"></div>
+          <div class="mobile-nav-content">
+              <div class="row">
+                <a href="/"><div class="nav-item nav-item-1 <?php if( $_SERVER['REQUEST_URI'] == "/") echo "active"; ?>">
+                <p>HOME</p>
+                </div></a><br />
+                <a href="/about"><div class="nav-item nav-item-2 <?php if( strpos($_SERVER['REQUEST_URI'],"/about") !== false)  echo "active"; ?>">
+                <p>ABOUT</p>
+                </div></a><br />
+                <a href="/library"><div class="nav-item nav-item-3 <?php if( strpos($_SERVER['REQUEST_URI'],"/library") !== false)  echo "active"; ?>">
+                <p>LIBRARY</p>
+                </div></a><br />
+                <a href="/calendar"><div class="nav-item nav-item-4 <?php if( strpos($_SERVER['REQUEST_URI'],"/calendar") !== false)  echo "active"; ?>">
+                <p>CALENDAR</p>
+                </div></a>
+              </div>
+          </div>
+      </div>
+  </div>
+-->
+  <!--mobile nav-menu-->
 
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle js-mobile-nav-click">
-        <span class="menu-trigger">
-        <span></span>
-        <span></span>
-        <span></span>
-        </span>
-        <!--
-        <span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        -->
-			</button>
-			<a class="navbar-brand" href="/">
-				inaminfo
-			</a>
-		</div>
-		<div class="collapse navbar-collapse" id="navbarEexample7">
-			<ul class="nav navbar-nav">
-				<li class="<?php if( $_SERVER['REQUEST_URI'] == "/") echo "active"; ?>"><a href="/">HOME</a></li>
-				<li class="<?php if( strpos($_SERVER['REQUEST_URI'],"/about") !== false)  echo "active"; ?>""><a href="/about">ABOUT</a></li>
-				<li class="<?php if( strpos($_SERVER['REQUEST_URI'],"/library") !== false)  echo "active"; ?>""><a href="/library">LIBRARY</a></li>
-				<li class="<?php if( strpos($_SERVER['REQUEST_URI'],"/calendar") !== false)  echo "active"; ?>""><a href="/calendar">CALENDAR</a></li>
-			</ul>
-		</div>
-	</div>
-</nav>
-<main>
-      <div class="container">
-        @yield('content')
+  <main>
+        <div id="container">
+          @yield('content')
+          </div>
+      </body>
+  </main>
+  <div class="page-footer">
+    <div class="container">
+      </br>
+      <div class="row">
+        <div class="col-md-8">
+          <p>当サイトは非公式ファンサイトです。</p>
         </div>
-    </body>
-</main>
-<div class="page-footer">
-  <div class="container">
-    </br>
-    <div class="row">
-      <div class="col-md-8">
-        <p>当サイトは非公式ファンサイトです。</p>
-      </div>
-      <div class="col-md-4">
-        <div class="list-group">
-          <a  href="http://smavoice.jp/" target="_blank">SMA VOICE</a>
-          </br>
-          <a  href="https://anjuinami.com/" target="_blank">伊波杏樹 Official Site</a>
-          </br>
-          <a  href="https://lineblog.me/anju_inami/" target="_blank">伊波杏樹 公式ブログ</a>
+        <div class="col-md-4">
+          <div class="list-group">
+            <a  href="http://smavoice.jp/" target="_blank">SMA VOICE</a>
+            </br>
+            <a  href="https://anjuinami.com/" target="_blank">伊波杏樹 Official Site</a>
+            </br>
+            <a  href="https://lineblog.me/anju_inami/" target="_blank">伊波杏樹 公式ブログ</a>
+          </div>
         </div>
       </div>
+      </br>
     </div>
-    </br>
   </div>
 </div>
+<script src=" {{ mix('js/app.js') }} "></script>
 </html>
